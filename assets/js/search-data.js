ba14ac516a028cd9ad20a9d91411f90397985999
ShoumikSaha <34313576+ShoumikSaha@users.noreply.github.com>
2025-01-21 12:38:26 +0000

    Deploying to gh-pages from @ ShoumikSaha/ShoumikSaha.github.io@351e562b86c0a57e31eb86cfbfeffd74bef1f4b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "news-fall-2024-completed-all-the-coursework-of-ph-d",
+          title: 'Fall 2024: Completed all the coursework of Ph.D.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-paper-co-authored-accepted-to-satml",
+          title: 'Paper (co-authored) accepted to SaTML!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
